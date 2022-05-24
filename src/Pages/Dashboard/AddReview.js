acd8ca2ff01dde538c1c3ecc3b83efe4de8e9263
dev-sibli <siblisadik13@gmail.com>
2022-05-24 23:13:17 +0600

    Add Review form created

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -3,13 +3,17 @@ import React from 'react';
 const AddReview = () => {
     return (
         <div>
-            <h1>Add a review</h1>
-            <div class="rating">
-                <input type="radio" name="rating-1" class="mask mask-star" />
-                <input type="radio" name="rating-1" class="mask mask-star" checked />
-                <input type="radio" name="rating-1" class="mask mask-star" />
-                <input type="radio" name="rating-1" class="mask mask-star" />
-                <input type="radio" name="rating-1" class="mask mask-star" />
+            <h1 className='text-2xl font-bold'>Add a review</h1>
+            <div class="form-control w-full max-w-xs">
+                <label class="label">
+                    <span class="label-text">Rating</span>
+                </label>
+                <input type="number" placeholder="Provide 1-5 Rating" class="input input-bordered w-full max-w-xs" />
+                <label class="label">
+                    <span class="label-text">Review</span>
+                </label>
+                <input type="text" placeholder="Enter Your Review" class="input input-bordered w-full max-w-xs" />
+                <input className='mt-4 btn w-full max-w-xs text-white ' type="submit" value="Add Review" />
             </div>
         </div>
     );
